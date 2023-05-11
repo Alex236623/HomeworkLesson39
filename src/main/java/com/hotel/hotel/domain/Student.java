c0d77ec97cba417928c512698eacc58612ae68de
Dalest <73247972+Alex236623@users.noreply.github.com>
2023-05-11 22:05:05 +0300

    Homework lesson 39

---
+++ src/main/java/com/hotel/hotel/domain/Student.java
@@ -0,0 +1,16 @@
+package com.hotel.hotel.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    public String firstName;
+    public String lastName;
+    public int age;
+}
+
+
